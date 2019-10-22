6908b805c244734e6c4acb8449b2459a1ee1399b
Inderpreet97 <inderpreet.singh1@studenti.unipr.it>
2019-10-22 12:51:21 +0200

    aggiunto Json.simple

--- lab02_15_10_2019/src/main/java/cantina_vini/Main.java
+++ lab02_15_10_2019/src/main/java/cantina_vini/Main.java
@@ -37,7 +37,87 @@ public class Main {
 		System.out.println("\n\nLogged out, see you soon!");
 
 	}
-
+	
+	public static void writeWineListOnFile() {
+		// TODO write wine list on 
+		//First Employee
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Gupta");
+        employeeDetails.put("website", "howtodoinjava.com");
+         
+        JSONObject employeeObject = new JSONObject();
+        employeeObject.put("employee", employeeDetails);
+         
+        //Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "Brian");
+        employeeDetails2.put("lastName", "Schultz");
+        employeeDetails2.put("website", "example.com");
+         
+        JSONObject employeeObject2 = new JSONObject();
+        employeeObject2.put("employee", employeeDetails2);
+         
+        //Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+         
+        //Write JSON file
+        try (FileWriter file = new FileWriter("employees.json")) {
+ 
+            file.write(employeeList.toJSONString());
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static void readWineListFromFile() {
+		// TODO read Wine List From File
+		//JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+         
+        try (FileReader reader = new FileReader("employees.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+ 
+            JSONArray employeeList = (JSONArray) obj;
+            System.out.println(employeeList);
+             
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+ 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	// TODO metod for reading JSON from file
+	private static void parseEmployeeObject(JSONObject employee)
+    {
+        //Get employee object within list
+        JSONObject employeeObject = (JSONObject) employee.get("employee");
+         
+        //Get employee first name
+        String firstName = (String) employeeObject.get("firstName");   
+        System.out.println(firstName);
+         
+        //Get employee last name
+        String lastName = (String) employeeObject.get("lastName"); 
+        System.out.println(lastName);
+         
+        //Get employee website name
+        String website = (String) employeeObject.get("website");   
+        System.out.println(website);
+    }
+	
 	public static void addUsers() {
 		// Add default users
 		Customer defaultCustomer1 = new Customer("beppe", "Giuseppe", "Urbano", "beppe123");
