6741c129361a9337b1efb06b40bdcc3f21d386d2
Inderpreet97 <inderpreet.singh1@studenti.unipr.it>
2020-01-20 15:42:34 +0100

    cose

--- lab04_12_11_2019/circolosportivo-JavaFX-DB/src/main/java/ing_software/circolosportivo_JavaFX_DB/Corso.java
+++ lab04_12_11_2019/circolosportivo-JavaFX-DB/src/main/java/ing_software/circolosportivo_JavaFX_DB/Corso.java
@@ -1,5 +1,10 @@
-package ing_software.circolo_sportivo;
+package ing_software.circolosportivo_JavaFX_DB;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Corso")
 public class Corso extends Attivita {
 
 	public Corso(String nome) {
