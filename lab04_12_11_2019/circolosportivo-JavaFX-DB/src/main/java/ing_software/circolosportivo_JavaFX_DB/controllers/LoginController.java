2ba72518933d79c7b1791474c20b73ff702e8bcb
GiuseppeUrbano <51412290+GiuseppeUrbano@users.noreply.github.com>
2020-01-24 16:58:41 +0100

    template fxml aggiunti login

--- lab04_12_11_2019/circolosportivo-JavaFX-DB/src/main/java/ing_software/circolosportivo_JavaFX_DB/controllers/LoginController.java
+++ lab04_12_11_2019/circolosportivo-JavaFX-DB/src/main/java/ing_software/circolosportivo_JavaFX_DB/controllers/LoginController.java
@@ -1,15 +1,46 @@
 package ing_software.circolosportivo_JavaFX_DB.controllers;
 
-import javafx.event.ActionEvent;
+import ing_software.circolosportivo_JavaFX_DB.LoginManager;
+import javafx.event.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
+/** Controls the login screen */
 public class LoginController {
-	@FXML
-	private Label actiontarget;
+  @FXML private TextField user;
+  @FXML private TextField password;
+  @FXML private Button loginButton;
+  
+  public void initialize() {}
+  
+  public void initManager(final LoginManager loginManager) {
+    loginButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent event) {
+        String sessionID = authorize();
+        if (sessionID != null) {
+          loginManager.authenticated(sessionID);
+        }
+      }
+    });
+  }
 
-	@FXML
-	protected void handleSubmitButtonAction(final ActionEvent event) {
-		actiontarget.setText("Sign in button pressed");
-	}
+  /**
+   * Check authorization credentials.
+   * 
+   * If accepted, return a sessionID for the authorized session
+   * otherwise, return null.
+   */   
+  private String authorize() {
+    return 
+      "open".equals(user.getText()) && "sesame".equals(password.getText()) 
+            ? generateSessionID() 
+            : null;
+  }
+  
+  private static int sessionID = 0;
+
+  private String generateSessionID() {
+    sessionID++;
+    return "xyzzy - session " + sessionID;
+  }
 }
